172d7027cf12a8d8f11f0c15d194b7eae50fd181
GechCodes <xblurayne30@gmail.com>
2023-03-13 05:23:48 -0700

    readme

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - Print value
+ * @s: input value
+ * Return: Nothing
+ */
+
+void _puts(char *s)
+{
+    puts(s);
+}
+
